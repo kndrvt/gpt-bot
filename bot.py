d57fbba6ba9e936fc1abb7f1c80af526f7da7293
Lev Pantyukhin <kndrvt@yandex.ru>
2023-11-16 21:51:04 +0000

    Adapt code to pylint

--- bot.py
+++ bot.py
@@ -1,26 +1,33 @@
-from telegram.ext import Application, MessageHandler
-from telegram.ext.filters import ALL
-from collections import defaultdict
+"""The simple Telegram bot to communicate with ChatGPT 3.5"""
+
+# pylint: disable=missing-function-docstring
+# pylint: disable=broad-exception-caught
+# pylint: disable=global-variable-not-assigned
+
 import logging
-import openai
 import os
 import time
+from collections import defaultdict
+
+import openai
+from telegram.ext import Application, MessageHandler
+from telegram.ext.filters import ALL
 
 history = defaultdict(list)
-max_history_size = 16
-max_tg_msg_size = 4096
-expiration_period = 30 * 60 # 30 munutes
+MAX_HISTORY_SIZE = 16
+MAX_TG_MSG_SIZE = 4096
+EXPIRATION_PERIOD = 30 * 60 # 30 munutes
 
 def append_history_entry(h, role, content):
     h.append((time.time(), {"role": role, "content": content}))
-    if len(h) > max_history_size:
-        del h[:-max_history_size]
+    if len(h) > MAX_HISTORY_SIZE:
+        del h[:-MAX_HISTORY_SIZE]
 
     now = time.time()
     for i, e in enumerate(h):
-        if e[0] + expiration_period > now:
-           del h[:i]
-           break
+        if e[0] + EXPIRATION_PERIOD > now:
+            del h[:i]
+            break
 
 async def generate_gpt_response(update, context):
     if not update.message:
@@ -33,7 +40,7 @@ async def generate_gpt_response(update, context):
     global history
     h = history[update.effective_chat.id]
 
-    logging.info("{} >: {}".format(update.effective_chat.id, user_input))
+    logging.info("%d >: %s", update.effective_chat.id, user_input)
 
     append_history_entry(h, "user", user_input)
     content = ""
@@ -47,32 +54,36 @@ async def generate_gpt_response(update, context):
         append_history_entry(h, response.choices[0].message.role, content)
     except Exception as exc:
         exc = str(exc)
-        delta = len("```ChatGPT API error: ```")
-        content = "```ChatGPT API error: {}```".format(exc if len(exc) < max_tg_msg_size - delta else exc[:max_tg_msg_size - delta])
+        delta = 25
+        content = f"```ChatGPT API error: \
+            {exc if len(exc) < MAX_TG_MSG_SIZE - delta else exc[:MAX_TG_MSG_SIZE - delta]}```"
         h.clear()
 
-    logging.info("{} <: {}".format(update.effective_chat.id, content))
-    logging.info("history size: {}".format(len(h)))
+    logging.info("%d <: %s", update.effective_chat.id, content)
+    logging.info("history size: %s", len(h))
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=content, parse_mode="MarkdownV2")
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=content,
+                                   parse_mode="MarkdownV2")
 
-def main(key, token):
-    logging.basicConfig(filename='bot.log', 
-                        format='%(asctime)s %(levelname)-5s %(message)s', 
-                        level=logging.INFO, 
+def main(k, t):
+    logging.basicConfig(filename='bot.log',
+                        format='%(asctime)s %(levelname)-5s %(message)s',
+                        level=logging.INFO,
                         datefmt='%Y-%m-%d %H:%M:%S')
-    openai.api_key = key
-    application = Application.builder().token(token).build()
+    openai.api_key = k
+    application = Application.builder().token(t).build()
     application.add_handler(MessageHandler(filters=ALL, callback=generate_gpt_response))
 
     try:
         application.run_polling()
     except Exception as ex:
-        logging.error("{}", ex)
+        logging.error("%s", ex)
 
 if __name__ == "__main__":
     key = os.getenv("OPENAI_API_KEY")
     token = os.getenv("TELEGRAM_API_TOKEN")
     if not key or not token:
-        raise Exception("Cannot start without initialization of OPENAI_API_KEY and TELEGRAM_API_TOKEN variables")
+        raise ValueError("Cannot start without initialization \
+                        of OPENAI_API_KEY and TELEGRAM_API_TOKEN variables")
     main(key, token)
